a8b535233c1940c4f65af053c8bf8fbb78528c0f
Igor Melo <igorandroid907@gmail.com>
2021-02-04 19:28:50 -0300

    Adding constants and default export to scraper

--- scraper.js
+++ scraper.js
@@ -1,18 +1,21 @@
 import axios from 'axios'
 import cheerio from 'cheerio'
 
-const COMMIT_URL_PREFIX = 'https://invent.kde.org/multimedia/kdenlive/-/commits'
+const KDENLIVE_URL = 'https://kdenlive.org'
+const KDENLIVE_BLOG = '/en/blog/'
+const INVENT_URL = 'https://invent.kde.org'
+const KDENLIVE_COMMITS = '/multimedia/kdenlive/-/commits/'
 
 async function getDOM(url) {
 	let res = await axios.get(url)
 	return cheerio.load(res.data)
 }
 
-async function getCommit(props = { offset: 0, urlPrefix: COMMIT_URL_PREFIX, branch: 'master' }) {
-	let url = `${props.urlPrefix}/${props.branch}`
+async function getCommit(props = { offset: 0, branch: 'master' }) {
+	let url = `${INVENT_URL}${KDENLIVE_COMMITS}/${props.branch}`
 	let $ = await getDOM(url)
 
-	let commitEl = $('.commit')[offset]
+	let commitEl = $('.commit')[props.offset]
 	let titleEl = $(commitEl).find('.commit-row-message.item-title')
 	let authorEl = $(commitEl).find('.commit-author-link')
 	
@@ -20,14 +23,16 @@ async function getCommit(props = { offset: 0, urlPrefix: COMMIT_URL_PREFIX, bran
 	let author = {}
 
 	commit.message = titleEl.text().trim()
-	commit.url = titleEl.attr('href')
+	commit.url = `${INVENT_URL}${titleEl.attr('href')}`
 	commit.shaShort = $(commitEl).find('.commit-sha-group').text().trim()
 	commit.datetime = $(commitEl).find('.js-timeago').attr('datetime')
 
 	author.name = authorEl.text().trim()
 	author.username = authorEl.attr("href").replace("/", "")
-	author.profileUrl = 'https://invent.kde.org/' + author.username
+	author.profileUrl = `${INVENT_URL}/${author.username}`
 
 	commit.author = author
 	return commit
 }
+
+export default { getCommit }
